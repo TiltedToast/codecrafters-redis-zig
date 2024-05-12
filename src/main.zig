cc1b6d334de048fffbcdd40e0d4518f22b363843
Tim <57221600+TiltedToast@users.noreply.github.com>
2024-05-12 20:33:21 +0200

    barebones ping command

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,10 @@ const net = std.net;
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     const address = try net.Address.resolveIp("127.0.0.1", 6379);
 
     var listener = try address.listen(.{
@@ -14,10 +18,23 @@ pub fn main() !void {
     try stdout.print("listening on {}\n", .{address});
 
     while (true) {
-        const connection = try listener.accept();
+        const conn = try listener.accept();
+        defer conn.stream.close();
 
-        try stdout.print("accepted new connection from {}\n", .{connection.address});
+        const writer = conn.stream.writer();
 
-        connection.stream.close();
+        try stdout.print("accepted new connection from {}\n", .{conn.address});
+
+        const message = conn.stream.reader().readUntilDelimiterAlloc(allocator, '\n', 1024) catch |err| {
+            try stdout.print("error reading message: {}\n", .{err});
+            continue;
+        };
+        defer allocator.free(message);
+
+        try stdout.print("received message: {s}\n", .{message});
+
+        if (std.mem.startsWith(u8, message, "PING")) {
+            _ = try writer.write("+PONG\r\n");
+        }
     }
 }
